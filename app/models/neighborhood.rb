a61391c28e9ca4793a4d17f85553864664c8870a
maryfarner18 <maryfarner@gmail.com>
2019-10-15 20:15:16 -0400

    Done.

--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -2,4 +2,42 @@ class Neighborhood < ActiveRecord::Base
   belongs_to :city
   has_many :listings
 
+  def neighborhood_openings(start_str, end_str)
+    #Not (A Or B) <=> Not A And Not B
+    #(StartA <= EndB)  and  (EndA >= StartB)
+    check_start = Date.parse(start_str)
+    check_end = Date.parse(end_str)
+    self.listings.select do |listing| 
+      listing.reservations.all? { |res| !(res.checkin <= check_end && res.checkout >= check_start)}
+    end
+    
+  end
+
+  def self.highest_ratio_res_to_listings
+    #return city with higest # of reservations per listing
+    best_avg = 0
+    best_neighborhood = nil
+    self.all.each do |neighborhood|
+      res_count = neighborhood.listings.map {|listing| listing.reservations.count}
+      avg = res_count.inject{ |sum, count| sum + count }.to_f / res_count.size
+      if avg > best_avg
+        best_avg = avg
+        best_neighborhood = neighborhood 
+      end
+    end
+
+    best_neighborhood
+
+  end
+
+  def get_reservations
+    self.listings.map {|listing| listing.reservations}.flatten
+  end
+
+  def self.most_res
+ 
+    self.all.max_by {|neighborhood| neighborhood.get_reservations.count}
+  
+  end
+
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,21 @@ class Review < ActiveRecord::Base
   belongs_to :reservation
   belongs_to :guest, :class_name => "User"
 
+  validates :rating, :description, :reservation_id, presence: true
+  validate :check_accepted, :reservation_passed
+
+  private
+
+  def check_accepted
+    if  self.reservation && self.reservation.status != "accepted" 
+      errors.add(:checkin, "Reservation must be accepted ")
+    end
+  end
+
+  def reservation_passed
+    if self.reservation && self.reservation.checkout && self.reservation.checkout > Date.today
+      errors.add(:checkin, "Reservation must be checked out")
+    end
+  end
+
 end
